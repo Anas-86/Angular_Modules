d71421c09782a43d1329449bd5fbdc6810479f7f
Anas-86 <anas.kala@hotmail.com>
2023-09-12 22:36:10 +0200

    lazy loading applied

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ElementsModule } from './elements/elements.module';
-import {CollectionsModule} from './collections/collections.module';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 @NgModule({
@@ -16,8 +13,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    ElementsModule,
-    CollectionsModule,
     AppRoutingModule
   ],
   providers: [],
