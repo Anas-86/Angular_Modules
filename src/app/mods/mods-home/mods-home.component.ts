beb47fd0d2115dd5077171d16ccb349a9e7a22b0
Anas <anas.kala@hotmail.com>
2023-10-04 13:55:34 +0200

    adding Accordion component to the module Mods

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -7,6 +7,11 @@ import { Component } from '@angular/core';
 })
 export class ModsHomeComponent {
   modalOpen = false;
+  items = [
+    { title: 'why is the sky blue?', content: 'The sky is blue because it is' },
+    { title: 'what does an orange taste like?', content: 'An orange tastes like orange' },
+    { title: 'what color is that cat?', content: 'That cat is an orange color' }
+  ];
 
   onClick() {
     this.modalOpen = !this.modalOpen;
